69e0cbe0fc844b3b5736cdcb074eab69ab32a711
Mahendra <MahendraC@TitanEd.com>
2025-02-02 18:27:01 +0530

    Updated theme branch

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -121,7 +121,7 @@ hooks.Filters.ENV_PATCHES.add_items(
            
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
-RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#master'
+RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#eduquest'
 
 """,
         )
@@ -133,14 +133,14 @@ RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#master
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#master'",
+        "RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#eduquest'",
     )
 )
 
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authoring",
-        "RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#master'",
+        "RUN npm install '@edx/brand@git+https://github.com/TitanEd/tels-brand.git#eduquest'",
     )
 )
 
